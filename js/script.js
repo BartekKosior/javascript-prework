a14a8949cbd73e6d7daee8d80284cbb28f6d6e10
Bartek Kosior <bartlomiejkosior2@gmail.com>
2021-03-28 14:15:57 +0200

    Add player move to script.js file

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
+//Wybór komputera
 let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
@@ -16,5 +17,25 @@ else if(randomNumber == 3){
     computerMove = 'nożyce';
 }
 
-
 printMessage('Mój ruch to: ' + computerMove);
+
+// Wybór gracza
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+
+else if(randomNumer == 2){
+    playerMove = 'papier';
+}
+
+else if(randomNumber == 3){
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
