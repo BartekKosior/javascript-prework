853ef0d14449547c92d08dc737571962c541c346
Bartek Kosior <bartlomiejkosior2@gmail.com>
2021-03-28 15:00:19 +0200

    Add player movements

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Losowanie liczby
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -26,16 +27,54 @@ console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = 'nieznany ruch';
 
-if(playerInput == '1'){
+if(playerInput == 1){
   playerMove = 'kamień';
 }
 
-else if(randomNumer == 2){
+else if(playerInput == 2){
     playerMove = 'papier';
 }
 
-else if(randomNumber == 3){
+else if(playerInput == 3){
     playerMove = 'nożyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+//Wynik gry
+if(computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz');
+}
+
+else(computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Ja wygrywam');
+}
+
+else(computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis');
+}
+
+if(computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Ja wygrywam');
+}
+
+else(computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis')
+}
+else(computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz')
+}
+
+if(computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis')
+}
+
+else(computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz')
+}
+
+else(computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Ja wygrywam')
+}
+
+
